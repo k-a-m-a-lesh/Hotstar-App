401a0607a4e551a1a92cf77e345214608335b270
Ganesh Sanap <114701526+ganeshsnp987@users.noreply.github.com>
2024-07-27 11:25:48 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ganeshsnp987" # Replace with your actual S3 bucket name
+    bucket = "ganeshsnp9871" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
